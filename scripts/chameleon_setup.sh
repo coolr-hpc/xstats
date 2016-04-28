743cfb73ad22773c09ed67b7001d027bfa110b6d
Kaicheng Zhang <kaicheng@users.noreply.github.com>
2016-04-28 13:03:38 -0500

    Update chameleon_setup.sh

--- scripts/chameleon_setup.sh
+++ scripts/chameleon_setup.sh
@@ -4,3 +4,6 @@ sudo yum update -y
 sudo yum install -y vim tmux freeipmi
 
 git clone https://github.com/coolr-hpc/xstats.git
+
+git config --global user.name "Kaicheng Zhang"
+git config --global user.email "kaichengz@gmail.com"
