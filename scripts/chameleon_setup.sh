be886b8db17133aa0d25d689ca5938a8188b87fe
Kaicheng Zhang <kaicheng@users.noreply.github.com>
2016-05-25 13:17:21 -0500

    update

--- scripts/chameleon_setup.sh
+++ scripts/chameleon_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install -y vim tmux freeipmi lm_sensors
+sudo yum install -y vim tmux freeipmi lm_sensors wget
+cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
 git clone https://github.com/coolr-hpc/xstats.git
 
